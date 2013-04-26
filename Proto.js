ecb75f6178e1ffbd2d05629295e88635689c38e2
Aaron Maenpaa <aaron@maenpaa.ca>
2013-04-26 16:39:41 -0400

    Initial commit.

---
+++ Proto.js
@@ -0,0 +1,50 @@
+﻿var Proto = (function () {
+    "use strict";
+
+    var methods = {
+        _super: function (base) {
+            var methods = Object.getPrototypeOf(base).__methods__;
+            var proxy = Object.create(this);
+            for (var key in methods) {
+                proxy[key] = $.proxy(methods[key], this);
+            }
+
+            return proxy;
+        },
+
+        create: function (properties) {
+            var model = Object.create(this);
+            model.__methods__ = Object.create(this.__methods__);
+            model.__methods__.extend.call(model, properties);
+            return model;
+        },
+
+        extend: function (properties) {
+            var key;
+            var propNames = properties ? Object.getOwnPropertyNames(properties) : [];
+            for (var i = 0; i < propNames.length; i++) {
+                var key = propNames[i];
+                var descriptor = Object.getOwnPropertyDescriptor(properties, key);
+
+                if (typeof descriptor.value === 'function') {
+                    this.__methods__[key] = descriptor.value;
+                    continue;
+                }
+
+                Object.defineProperty(this, key, descriptor);
+            }
+
+            for (key in this.__methods__) {
+                this[key] = $.proxy(this.__methods__[key], this);
+            }
+        }
+    };
+
+    var Proto = {
+        __name__: "Proto",
+        __methods__: {}
+    };
+
+    methods.extend.call(Proto, methods);
+    return Proto;
+} ());
---
+++ tests.js
@@ -0,0 +1,156 @@
+﻿var fb = fb || {};
+
+$(function () {
+    "use strict";
+
+    module("Proto.js");
+
+	function fmt(tmpl) {
+		var dict = arguments[arguments.length - 1];
+		var args = Array.prototype.slice.call(arguments, 1);
+		return tmpl.replace(/{([^}]+)}/g, function(_, key) {
+			var idx = parseInt(key, 10);
+			if (isNaN(idx)) {
+				return dict[key];
+			}
+			return args[idx];	
+		});
+	}
+
+    var Person = Proto.create({
+        hello: function () {
+            return fmt("Hello my name is {name}.", this);
+        },
+        say: function (name) {
+            return fmt("Hello {0}!", name);
+        },
+        sayItWithFeeling: function (name, attitude) {
+            return fmt("Hello {0} it is a {1} day!", name, attitude);
+        },
+        get fullName() {
+            return fmt("{0} {1}", this.name, this.lastName);
+        },
+        set fullName(name) {
+            var names = name.split(" ");
+            this.name = names[0];
+            this.lastName = names[1];
+        }
+    });
+
+    var ClassyPerson = Person.create({
+        say: function (name) {
+            return "Indubitably.";
+        },
+        get fullName() {
+            return fmt("{1}, {0} {1}", this.name, this.lastName);
+        }
+    });
+
+    var QuietPerson = Person.create({
+        hello: function () {
+            var msg = this._super(QuietPerson).hello();
+            return msg.toLowerCase();
+        }
+    });
+
+    var LoudPerson = QuietPerson.create({
+        hello: function () {
+            var msg = this._super(LoudPerson).hello();
+            return msg.toUpperCase();
+        },
+        sayItWithFeeling: function (name, attitude) {
+            var msg = this._super(LoudPerson).sayItWithFeeling(name, attitude);
+            return msg.toUpperCase();
+        }
+    });
+
+    var OpinonatedPerson = Person.create({
+        create: function (name) {
+            return this._super(OpinonatedPerson).create({ name: name });
+        }
+    });
+
+    test("Proto objects", function () {
+        equal(
+            Person.say("world"), "Hello world!",
+            "can have useful methods"
+        );
+
+        equal(
+            ClassyPerson.say("world"), "Indubitably.",
+            "can override inherited methods"
+        );
+
+        var person = Person.create({ name: "Alan" });
+        equal(
+            person.hello(), "Hello my name is Alan.",
+            "methods can referance instance data"
+        );
+
+        equal(
+            person.hello.apply(null), "Hello my name is Alan.",
+            "methods are bound when they're created"
+        );
+
+        var quiet = QuietPerson.create({ name: "Alan" });
+        equal(
+            quiet.hello(), "hello my name is alan.",
+            "overrides can access overriden methods using _super()"
+        );
+
+        var loud = LoudPerson.create({ name: "Alan" });
+        equal(
+            loud.hello(), "HELLO MY NAME IS ALAN.",
+            "_super() chain goes all the way to the root"
+        );
+
+        equal(
+            loud.sayItWithFeeling("world", "wonderful"),
+            "HELLO WORLD IT IS A WONDERFUL DAY!",
+            "_super() lets you pass along arguments"
+        );
+
+        LoudPerson.extend({
+            say: function (name) {
+                var msg = this._super(LoudPerson).say(name);
+                return msg.toUpperCase();
+            }
+        });
+
+        var evenLouder = LoudPerson.create();
+        equal(
+            evenLouder.say("world"), "HELLO WORLD!",
+            "are open an can be extended with extend()"
+        );
+
+        equal(
+            loud.say("world"), "Hello world!",
+            "but existing objects' methods are already bound"
+        );
+
+        var anon = OpinonatedPerson.create("Anon Y. Mouse");
+        equal(
+            anon.hello(), "Hello my name is Anon Y. Mouse.",
+            "can override create() to control object construction"
+        );
+
+        var fullNamed = LoudPerson.create({name: "John", lastName: "Doe"});
+        equal(
+            fullNamed.fullName, "John Doe",
+            "accessor properties work and bind to the bottom-most 'this' object"
+        );
+
+        var jamesBond = ClassyPerson.create({name: "James", lastName: "Bond"});
+        equal(
+            jamesBond.fullName, "Bond, James Bond",
+            "you can override accessors... but don't call _super()"
+        );
+
+        var noName = Person.create({});
+        noName.fullName = "John Doe";
+        equal(
+            fmt("{name}:{lastName}", noName), "John:Doe",
+            "setters work too"
+        );
+    });
+});
